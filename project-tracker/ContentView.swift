601f997a8fad92ea935e1b703123de290f994438
Joe Miehl <joe.miehl@gmail.com>
2024-03-20 18:53:53 -0400

    Initial Commit

---
+++ project-tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  project-tracker
+//
+//  Created by Joe Miehl on 3/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project-tracker/project_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_trackerApp.swift
+//  project-tracker
+//
+//  Created by Joe Miehl on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct project_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
