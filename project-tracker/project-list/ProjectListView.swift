e0121af1c2540dd1270b0d6f09c6fdafc0d3548e
Joe Miehl <joe.miehl@gmail.com>
2024-03-23 18:30:19 -0400

    - add documentation for     - project list view     - project tracker app

--- project-tracker/project-list/ProjectListView.swift
+++ project-tracker/project-list/ProjectListView.swift
@@ -10,27 +10,34 @@ import SwiftData
 struct ProjectListView: View {
     
     
-    @State private var newProject: Project?
-    @Query private var projects: [Project]
+    @State private var newProject: Project? // variable to bind to sheet details
+    @Query private var projects: [Project] //obtains the data for projects
     
     var body: some View {
         NavigationStack {
             ZStack {
+                // sets the background color using a gradient
                 LinearGradient(colors: [Color("Deep Purple"), Color("Blush Pink")], startPoint: .top, endPoint: .bottom)
                     .ignoresSafeArea()
                 
                 VStack (alignment: .leading) {
+                    
+                    // sets the title and formating
                     Text("Projects")
                         .font(.screenHeading)
                         .foregroundStyle(Color.white)
                     
                     ScrollView (showsIndicators: false) {
+                        
+                        // displays the projects
                         VStack (alignment: .leading, spacing: 26){
                             ForEach(projects) { p in
-                                
+                                    // using navigation link allows for moving to project detail
                                 NavigationLink {
+                                    // displays the project detail view
                                     ProjectDetailView(project: p)
                                 } label: {
+                                    // displays the poject card view
                                     ProjectCardView(project: p)
                                 }
                                 .buttonStyle(.plain)
@@ -48,6 +55,7 @@ struct ProjectListView: View {
                         self.newProject = Project()
                     }, label: {
                         ZStack {
+                            // builds a custom button
                             Circle()
                                 .frame(width: 65)
                                 .foregroundColor(.black)
@@ -58,6 +66,7 @@ struct ProjectListView: View {
                 .padding()
             }
         }
+        //displays a quarter sheet to create a new project based on binded variable $newProject
         .sheet(item: $newProject) { project in
             AddProjectView(project: project)
                 .presentationDetents([.fraction(0.20)])
--- project-tracker/project_trackerApp.swift
+++ project-tracker/project_trackerApp.swift
@@ -12,6 +12,8 @@ import SwiftData
 struct project_trackerApp: App {
     var body: some Scene {
         WindowGroup {
+            // add project entry point, add .modelContainer to save project and project update
+            
             ProjectListView()
                 .modelContainer(for: [Project.self, ProjectUpdate.self])
         }
